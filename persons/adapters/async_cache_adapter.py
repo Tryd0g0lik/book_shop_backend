2b882347fad0c25d94d5c31439727c1d9a94b24e
Denis <work80@mail.ru>
2026-05-14 12:42:55 +0700

    [dev]: upgrad the adapters. Additional base ane async cache adapters added. and Test test_cache_manager_saving.py was added

---
+++ persons/adapters/async_cache_adapter.py
@@ -0,0 +1,224 @@
+"""
+persons/adapters/async_cache_adapter.py:1
+"""
+
+import asyncio
+import logging
+import re
+from contextlib import asynccontextmanager
+from typing import Optional
+
+from redis.asyncio import ConnectionError as AsyncConnectionError
+from redis.asyncio import Redis, RedisError
+from redis.asyncio.connection import ConnectionPool
+
+from persons.adapters.cache_base import CacherBase
+
+log = logging.getLogger(__name__)
+
+
+class AsyncCacherAdapter(CacherBase):
+    __async_pool_lock = asyncio.Lock()
+    async_pool: Optional[ConnectionPool] = None
+
+    async def _init_pool(self) -> None:
+
+        from project.settings_conf.settings_env import (
+            REDIS_HOST,
+            REDIS_PORT,
+        )
+
+        try:
+            if AsyncCacherAdapter.async_pool is None:
+                # ============================================
+                # GET WORKERS
+                # ============================================
+                async with AsyncCacherAdapter.__async_pool_lock:
+                    AsyncCacherAdapter.async_pool = ConnectionPool(
+                        host=REDIS_HOST,
+                        port=int(REDIS_PORT),
+                        password=self.redis_password,
+                        username=self.redis_master_name,
+                        db=self.redis_database,
+                        max_connections=self.max_connections,
+                        decode_responses=self.decode_responses,
+                        socket_connect_timeout=self.socket_connect_timeout,
+                        socket_timeout=self.socket_timeout,
+                        retry_on_timeout=self.retry_on_timeout,
+                        health_check_interval=self.health_check_interval,
+                    )
+                    log.info(self.log_t + " Redis Connection pool initialized.")
+        except Exception as e:
+            log_t = self.log_t[
+                :-1
+            ] + " Connection with a cache server failed. %s" % str(e)
+            raise ValueError(log_t)
+
+    async def __get_client(self):
+        if AsyncCacherAdapter.async_pool is None:
+            async with AsyncCacherAdapter.__async_pool_lock:
+                await self._init_pool()
+        return await Redis(connection_pool=AsyncCacherAdapter.async_pool)
+
+    async def related(self) -> bool:
+        if self.server_client is None:
+            try:
+                # ============================================
+                # CONNECT TO THE CACHE SERVER
+                # ============================================
+                self.server_client = await self.__get_client()
+            except Exception as e:
+                log_t = (
+                    self.log_t
+                    + " Connection with a cache server failed. %s" % e.args[0]
+                    if e.args
+                    else str(e)
+                )
+                raise ValueError(log_t)
+        return True
+
+    @asynccontextmanager
+    async def connected(self):
+        is_connected = await self.is_connected
+        if not is_connected:
+            log_t = self.log_t + " Connection with a cache server is invalid."
+            raise ValueError(log_t)
+        # ============================================
+        # GET REDIS CLIENT
+        # ============================================
+        server_client = self.server_client
+        try:
+            await server_client.ping()
+            yield server_client
+        except AsyncConnectionError as e:
+            log_t = self.log_t[
+                :-1
+            ] + " AsyncConnectionError. Connection with a cache server is closed.%s" % str(
+                e
+            )
+            raise AsyncConnectionError(log_t) from e
+
+        except TimeoutError as e:
+            log_t = self.log_t[
+                :-1
+            ] + " TimeoutError error. Connection with a cache server is closed.%s" % str(
+                e
+            )
+            raise TimeoutError(log_t) from e
+        except RedisError as e:
+            log_t = self.log_t + "Mistake on a cache server: %s" % str(e)
+            raise RedisError(log_t) from e
+
+        except Exception as e:
+            log_t = self.log_t + e.args[0] if e.args else str(e)
+            raise ValueError(log_t)
+        finally:
+            pass
+
+    async def _recreated_pool(self):
+        async with AsyncCacherAdapter.__async_pool_lock:
+            is_pool = AsyncCacherAdapter.async_pool
+            if is_pool is not None:
+                await AsyncCacherAdapter.async_pool.disconnect()
+                AsyncCacherAdapter.async_pool = None
+            await self._init_pool()
+
+    async def close(self):
+        # ============================================
+        # EVERY CONNECTION WILL BE CLOSED
+        # ============================================
+
+        if self.server_client:
+            self.server_client.close()
+            self.server_client = None
+        is_pool = AsyncCacherAdapter.async_pool
+        if is_pool is not None:
+            await AsyncCacherAdapter.async_pool.disconnect()
+            AsyncCacherAdapter.async_pool = None
+
+    @property
+    async def is_connected(self) -> bool:
+        is_pool = AsyncCacherAdapter.async_pool
+        if is_pool is None:
+            return False
+        try:
+            client = self.server_client
+            if client is None:
+                return False
+
+            await client.ping()
+            return True
+        except AsyncConnectionError as e:
+            log.error(
+                AsyncConnectionError(
+                    self.log_t
+                    + " Connection with a cache server is invalid. AsyncConnectionError: %s"
+                    % e.args[0]
+                    if e.args
+                    else str(e)
+                )
+            )
+            return False
+        except TimeoutError as e:
+            log.error(
+                TimeoutError(
+                    self.log_t
+                    + " Connection with a cache server is invalid. TimeoutError: %s"
+                    % e.args[0]
+                    if e.args
+                    else str(e)
+                )
+            )
+            return False
+        except RedisError as e:
+            log.error(
+                RedisError(
+                    self.log_t
+                    + " Connection with a cache server is invalid. RedisError: %s"
+                    % e.args[0]
+                    if e.args
+                    else str(e)
+                )
+            )
+            return False
+        except Exception as e:
+            log.error(
+                self.log_t
+                + " Connection with a cache server failed. %s"
+                % str(e.args[0] if e.args else str(e))
+            )
+            return False
+
+    @property
+    def redis_password(self) -> str:
+        return self.__redis_password
+
+    @redis_password.setter
+    def redis_password(self, line: str) -> None:
+        if isinstance(line, str) and len(line) == 0:
+            log_t = self.log_t + " Password is invalid."
+            raise ValueError(log_t)
+        self.__redis_password = line
+
+    @property
+    def redis_master_name(self) -> str:
+        return self.__redis_master_name
+
+    @redis_master_name.setter
+    def redis_master_name(self, line: str) -> None:
+        if (
+            isinstance(line, str)
+            and len(line) == 0
+            and re.match(r"\w{1,50}", line, flags=re.ASCII)
+        ):
+            log_t = self.log_t + " User name is invalid."
+            raise ValueError(log_t)
+        self.__redis_master_name = line
+
+    @property
+    def redis_database(self) -> int:
+        return self.__redis_db
+
+    @redis_database.setter
+    def redis_database(self, num: int) -> None:
+        self.__redis_db = num
---
+++ persons/adapters/cache_base.py
@@ -0,0 +1,54 @@
+"""
+persons/adapters/cache_base.py:1
+"""
+
+from typing import Optional
+
+from redis import Redis
+
+
+class CacherBase:
+
+    def __init__(
+        self,
+        db: int = 0,
+        max_connections: int = 10,
+        decode_responses: bool = False,
+        socket_connect_timeout: int = 5,
+        socket_timeout: int = 5,
+        retry_on_timeout: bool = True,
+        health_check_interval: int = 30,
+    ) -> None:
+        """
+        This is the sync code.
+        :param int db: This is integer nuber. That is number of the redis's db. Default value is 0.
+        :param: Optional[str] redis_master_name. This is a string this is a name/login of the cache server. Default value is None.
+        :param: Optional[str] redis_password. This is a string this is a password of the cache server. Default value is None.
+        :param: method related. Connection with a cache server.
+        :param: method closed. This method is close connection and assign 'self.server_caching = None'
+        :param  connected has wrapper the contextmanager. Exemple: ```
+            cache = CacheAdapter()
+            cache.redis_password = "<PASSWORD>" # if required.
+            cache.redis_master_name = "<LOGIN / USERNAME>" # if required.
+            cache.related()
+            try:
+                with cache.connected() as conn:
+                    # ...
+            finally:
+                cache.close()
+
+        ```
+        :param redis_master_name:
+        """
+        self.__redis_password: Optional[str] = None
+        self.__redis_db: int = db
+        self.__redis_master_name: Optional[str] = None
+        self.server_client: Optional[Redis] = None
+        self.log_t = "[%s]:" % CacherBase.__class__.__name__
+
+        self.max_connections: int = max_connections
+        self.decode_responses: bool = decode_responses
+        self.socket_connect_timeout: int = socket_connect_timeout
+        self.socket_timeout: int = socket_timeout
+        self.retry_on_timeout: bool = retry_on_timeout
+        self.health_check_interval: int = health_check_interval
