e3b074ff83862baa59211c0460d62b554a2471a1
Denis <work80@mail.ru>
2026-05-17 04:21:25 +0700

    [dev]: Class was renamed, before was the name 'CacherBase' and after/now is the 'CacherBaserMixin'

--- persons/adapters/cache_base.py
+++ persons/adapters/cache_base.py
@@ -2,12 +2,13 @@
 persons/adapters/cache_base.py:1
 """
 
+import re
 from typing import Optional
 
 from redis import Redis
 
 
-class CacherBase:
+class CacherBaseMixin:
 
     def __init__(
         self,
@@ -44,7 +45,7 @@ class CacherBase:
         self._redis_db: int = db
         self._redis_master_name: Optional[str] = None
         self.server_client: Optional[Redis] = None
-        self.log_t = "[%s]:" % CacherBase.__class__.__name__
+        self.log_t = "[%s]:" % CacherBaseMixin.__class__.__name__
 
         self.max_connections: int = max_connections
         self.decode_responses: bool = decode_responses
@@ -52,3 +53,37 @@ class CacherBase:
         self.socket_timeout: int = socket_timeout
         self.retry_on_timeout: bool = retry_on_timeout
         self.health_check_interval: int = health_check_interval
+
+    @property
+    def redis_password(self) -> str:
+        return self._redis_password
+
+    @redis_password.setter
+    def redis_password(self, line: str) -> None:
+        if line is not None or isinstance(line, str) and len(line) == 0:
+            log_t = self.log_t + " Password is invalid."
+            raise ValueError(log_t)
+        self._redis_password = line
+
+    @property
+    def redis_master_name(self) -> str:
+        return self._redis_master_name
+
+    @redis_master_name.setter
+    def redis_master_name(self, line: str) -> None:
+        if (
+            isinstance(line, str)
+            and len(line) == 0
+            and re.match(r"\w{1,50}", line, flags=re.ASCII)
+        ):
+            log_t = self.log_t + " User name is invalid."
+            raise ValueError(log_t)
+        self._redis_master_name = line
+
+    @property
+    def redis_database(self) -> int:
+        return self._redis_db
+
+    @redis_database.setter
+    def redis_database(self, num: int) -> None:
+        self._redis_db = num
