c1146f378ad3632410e20d17c0d74001f45a7662
Denis <work80@mail.ru>
2026-05-27 08:55:22 +0700

    [dev]: THe new README.md was added

--- persons/interfaces/__init__.py
+++ persons/interfaces/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ["UsersPydantic", "EmailString"]
+__all__ = ["UsersPydantic", "EmailString", "PersonServiceInitialize"]
 
+from persons.interfaces.interface_dataservice import PersonServiceInitialize
 from persons.interfaces.interface_emailStr import EmailString
 from persons.interfaces.interface_persons import UsersPydantic
