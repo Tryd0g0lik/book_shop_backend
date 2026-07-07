6f8c5bb35ac41b5af7332a15554572f725fa674f
Denis <work80@mail.ru>
2026-07-07 13:46:35 +0700

    [dev]; 'catalog/permisions/permisions_checker.py' added & ' utilities/permisions.py' updated

--- utilities/permisions.py
+++ utilities/permisions.py
@@ -1,18 +1,32 @@
+from typing import Optional
+
 from persons.interfaces import Users
 
 
 class PermissionsMixin:
+    @staticmethod
+    def is_active(user: Optional[Users]) -> bool:
+        if user is None:
+            return False
+        if (
+            user is not None
+            and getattr(user, "is_verified")
+            and getattr(user, "is_active")
+        ):
+            return True
+        return False
 
     @staticmethod
-    def is_anonymous(user: Users) -> bool:
+    def is_anonymous(user: Optional[Users]) -> bool:
         return getattr(user, "is_anonymous")
 
     @staticmethod
-    def is_authenticated(user: Users) -> bool:
+    def is_authenticated(user: Optional[Users]) -> bool:
         return getattr(user, "is_authenticated")
 
     @staticmethod
-    def is_admin(user: Users) -> bool:
+    def is_admin(user: Optional[Users]) -> bool:
+
         return getattr(user, "is_superuser")
 
     @staticmethod
@@ -34,9 +48,3 @@ class PermissionsMixin:
     def is_client(user: Users) -> bool:
         group = getattr(user, "group")
         return group.name.lower() == "client"
-
-    @staticmethod
-    def is_active(user: Users) -> bool:
-        if getattr(user, "is_verified") and getattr(user, "is_active"):
-            return True
-        return False
